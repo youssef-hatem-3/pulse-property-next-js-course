7678879c094409e15e75e9f63d928c31e7f3ef9b
youssefm1 <youssef.m@network.global>
2025-10-02 08:42:21 +0300

    adding properities details and details image header

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,14 +1,35 @@
-import PropertyHeaderImage from '@/components/propertyHeaderImage/PropertyHeaderImage.jsx'
-import React from 'react'
-import connectDB from '@/config/database';
-import Property from '@/models/Property';
+import PropertyHeaderImage from "@/components/propertyHeaderImage/PropertyHeaderImage.jsx";
+import React from "react";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+import PropertyDetails from "@/components/PropertyDetails/PropertyDetails";
 
-
- const propertyDetails = async({params}) => {
-    await connectDB();
-    const property = await Property.findById(params.id);
+const propertyDetails = async ({ params }) => {
+  await connectDB();
+  const property = await Property.findById(params.id);
   return (
-    <div><PropertyHeaderImage image={property.images[0]}/></div>
-  )
-}
-export default propertyDetails
+    <div>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2"/> Back to Properties
+          </Link>
+        </div>
+        <section className="bg-blue-50">
+      <div className="container m-auto py-10 px-6">
+        <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={property}/>
+        </div>
+      </div>
+    </section>
+      </section>
+    </div>
+  );
+};
+export default propertyDetails;
